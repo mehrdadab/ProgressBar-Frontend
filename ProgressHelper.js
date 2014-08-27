1ed9913866062e0fd0525a85a58f84487a95d4f7
mehrdadab <mehrdadab@yahoo.com>
2014-08-27 16:28:29 +1000

    This is my code commited

---
+++ ProgressHelper.js
@@ -0,0 +1,31 @@
+﻿var helper = window.helper || {};
+
+helper.ProgressController = function () {
+    /////////////////////Private functions//////////////////////////////////
+    var checkProgressForWarning = function (progressId, btnValue) {
+            var progressbar = $("#" + progressId);
+            var progressbarValue = progressbar.find(".ui-progressbar-value");
+        if (parseInt(btnValue) > 100) {
+            progressbarValue.css({ "background": "red" });
+        }
+        else {
+            progressbarValue.css({"background": "#44FF44"});
+        }
+    }
+    ///////////Public functions/////////////////////////////////////////
+    return {
+        updateProgress: function (progressId, btnValue) {
+
+            var progressCurrentVal = $("#" + progressId).progressbar("value");
+
+            var progressNewVal = progressCurrentVal + btnValue;
+
+            $("#" + progressId).progressbar("value", parseInt(progressNewVal));
+
+            $("#" + progressId + ">.progress-label").text(progressNewVal + "%");
+
+            checkProgressForWarning(progressId, progressNewVal);
+        }
+        ////////////////////////////////////////////////////////////////////
+    }
+}
